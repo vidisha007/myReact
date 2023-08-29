a21faf79015399e0d81c824f519e486bc2168642
vidisha007 <vidisha.pandey00@gmail.com>
2023-08-29 21:45:10 +0545

    fixed

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 
 //react element
-const heading = (
+const Heading = (
     <h1 className="head" tabIndex = "5">
         Namaste react using jsx
     </h1>    
 )
 
 //react component
-const FirstFun = ()=>{
+const FirstFun = ()=>
     <h1>Iam a small react component</h1>
-}
+
 
 const ReactHeading = ()=>{
- <div>
-{heading}
+return <div>
+{Heading}
 <FirstFun/>
 <h2>I am a React Heading</h2>
 </div>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<ReactHeading/>);
 
